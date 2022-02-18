4c893b3b6d156570cc1afa84ca8e1b2026e61752
J. B. Rainsberger <me@jbrains.ca>
2022-02-18 13:34:42 -0400

    [bookmark] We are trying to sell an item with a different price, but we can't do that yet.

--- lib/src/test/java/ca/jbrains/pos/test/SellOneItemTest.java
+++ lib/src/test/java/ca/jbrains/pos/test/SellOneItemTest.java
@@ -14,6 +14,16 @@ public class SellOneItemTest {
         Assertions.assertEquals("$7.95", display.getText());
     }
 
+    @Test
+    void anotherProductFound() {
+        final Sale sale = new Sale();
+        final Display display = new Display();
+
+        sale.onBarcode("23456");
+
+        Assertions.assertEquals("$12.50", display.getText());
+    }
+
     public static class Sale {
         public void onBarcode(String barcode) {
         }
