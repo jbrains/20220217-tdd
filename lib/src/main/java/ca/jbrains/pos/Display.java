d68b39b3ee369062862437e77c1f7e619a1392ca
J. B. Rainsberger <me@jbrains.ca>
2022-02-24 19:26:12 -0400

    Split a text template into two parts in order to reuse formatting an amount.

--- lib/src/main/java/ca/jbrains/pos/Display.java
+++ lib/src/main/java/ca/jbrains/pos/Display.java
@@ -20,7 +20,7 @@ public class Display {
     }
 
     public void displayTotal(int totalInCents) {
-        this.text = String.format("Total: $%.2f", amountInDollars(totalInCents));
+        this.text = String.format("Total: %s", String.format("$%.2f", amountInDollars(totalInCents)));
     }
 
     private double amountInDollars(int totalInCents) {
